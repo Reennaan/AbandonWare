b004a49eff82a334e04fcfb8fb3012182e9cc4bd
Renan <qwerty142999@gmail.com>
2025-05-04 20:12:50 -0300

    hihih

--- src/main/java/com/example/demo/Controller/TaskController.java
+++ src/main/java/com/example/demo/Controller/TaskController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.Model.Table;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -66,18 +68,50 @@ public class TaskController {
 
     @GetMapping("/gamePage")
     public String redirectToGame(Model model, @RequestParam String link, @RequestParam String gameName) throws IOException{
-        System.out.println(link);
+        
         
         Document doc = Jsoup.connect(link).timeout(90000).get();
+        Document docDownload = Jsoup.connect(link+"#download").timeout(90000).get();
         Elements tableInfo = doc.select(".gameInfo");
         Elements description = doc.select(".gameDescription");
-        Elements links = doc.select(".extLinks");
+        if(description.isEmpty()){ 
+            description = doc.select(".gameInstruction");
+        }
         Elements snapshots = doc.select(".screens");
+        String download = doc.select("a[href='#download'] > span").text(); //muito útil
+
+        Table table = new Table();
+
+        table.setAltNames(tableInfo.select("tbody > tr:nth-of-type(1) > td").text());
+        table.setYear(tableInfo.select("tbody > tr:nth-of-type(2) > td").text());
+        table.setPlatform(tableInfo.select("tbody > tr:nth-of-type(3) > td").text());
+        table.setReleased(tableInfo.select("tbody > tr:nth-of-type(4) > td").text());
+        table.setGenre(tableInfo.select("tbody > tr:nth-of-type(5) > td").text());
+        table.setTheme(tableInfo.select("tbody > tr:nth-of-type(6) > td").text());
+        table.setPublisher(tableInfo.select("tbody > tr:nth-of-type(7) > td").text());
+        table.setDeveloper(tableInfo.select("tbody > tr:nth-of-type(8) > td").text());
+        table.setPerspectives(tableInfo.select("tbody > tr:nth-of-type(9) > td").text());
+        table.setTestedOn(tableInfo.select("tbody > tr:nth-of-type(10) > td").text());
+        
+        model.addAttribute("altName", table.getAltNames());
+        model.addAttribute("year", table.getYear());
+        model.addAttribute("platform", table.getPlatform());
+        model.addAttribute("released", table.getReleased());
+        model.addAttribute("genre", table.getGenre());
+        model.addAttribute("theme", table.getTheme());
+        model.addAttribute("publisher", table.getPublisher());
+        model.addAttribute("developer", table.getDeveloper());
+        model.addAttribute("perspectives", table.getPerspectives());
+        model.addAttribute("testedOn", table.getTestedOn());
+
+        System.out.println(download);
 
         model.addAttribute("gameDescription", description);
-        model.addAttribute("tab", tableInfo);
-        model.addAttribute("links", links );
-        model.addAttribute("snapshots", snapshots);
+        model.addAttribute("gameName", gameName);
+        model.addAttribute("download", download);
+        //model.addAttribute("tab", tableInfo);
+        //model.addAttribute("links", links );
+        //model.addAttribute("snapshots", snapshots);
 
 
         return "gamePage";
---
+++ src/main/java/com/example/demo/Model/Table.java
@@ -0,0 +1,128 @@
+package com.example.demo.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Table {
+ 
+    String altNames; 
+    String year;
+    String platform;
+    String released; 
+    String genre; 
+    String theme;
+    String publisher; 
+    String developer; 
+    String perspectives; 
+    String testedOn; 
+
+
+    public Table(){
+
+    }
+
+
+    public String getAltNames() {
+        return altNames;
+    }
+
+
+    public void setAltNames(String altNames) {
+        this.altNames = altNames;
+    }
+
+
+    public String getYear() {
+        return year;
+    }
+
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+
+    public String getPlatform() {
+        return platform;
+    }
+
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+
+    public String getReleased() {
+        return released;
+    }
+
+
+    public void setReleased(String released) {
+        this.released = released;
+    }
+
+
+    public String getGenre() {
+        return genre;
+    }
+
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+
+    public String getTheme() {
+        return theme;
+    }
+
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+
+    public String getPerspectives() {
+        return perspectives;
+    }
+
+
+    public void setPerspectives(String perspectives) {
+        this.perspectives = perspectives;
+    }
+
+
+    public String getTestedOn() {
+        return testedOn;
+    }
+
+
+    public void setTestedOn(String testedOn) {
+        this.testedOn = testedOn;
+    }
+
+    
+    
+
+}
