f1f462d8a1ad19df503160809e4ee76fc7025553
Renan <qwerty142999@gmail.com>
2025-05-14 22:16:51 -0300

    almost

--- src/main/java/com/example/demo/Controller/TaskController.java
+++ src/main/java/com/example/demo/Controller/TaskController.java
@@ -135,9 +135,41 @@ public class TaskController {
     }
 
     @GetMapping("/search/q/{name}")
-    public String search(@PathVariable String name , Model model){
+    public String search(@PathVariable String name , Model model) throws IOException{
+        Document doc  = Jsoup.connect(url+ "/search/q/"+name).timeout(90000).get();
+         Elements listOfGame = doc.select(".itemListGame");
+        Elements gameImgList = listOfGame.select(".c-thumb__img");
+        Elements gameNameList = listOfGame.select(".c-item-game__name");
+        Elements gamePlatformsList = listOfGame.select(".c-item-game__platforms");
+        Elements gameYearList = listOfGame.select(".c-item-game__year");
+
+
+
+
+        List<String> nameList = new ArrayList<>();
+        List<String> linkList = new ArrayList<>();
+        List<String> dateList = new ArrayList<>();
+        List<String> imgList = new ArrayList<>();
+        List<String> platformList = new ArrayList<>();
+
+
+       for(int i = 1; i < gameNameList.size() ; i++){
+           nameList.add(gameNameList.get(i).text());
+           imgList.add(url+gameImgList.get(i).attr("src"));
+           linkList.add(url+gameNameList.get(i).attr("href"));
+           dateList.add(gameYearList.get(i).text());
+           platformList.add(gamePlatformsList.get(i).text());
+       }
         
 
+        
+       model.addAttribute("link", linkList);
+       model.addAttribute("imgSrc",imgList);
+       model.addAttribute("platform",platformList);
+       model.addAttribute("gameName", nameList);
+       model.addAttribute("searchName",name);
+       
+        
 
 
         return "search";
